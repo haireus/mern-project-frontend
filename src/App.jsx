f1a85d26f7c332508418cb12297bd8a9d4eba6ee
hai.dinh <hai.dinh@amela.vn>
2020-10-08 08:46:44 +0700

    working on userlist

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  BrowserRouter as Router, Redirect, Route, Switch,
+} from 'react-router-dom';
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
